ec20140e28205636fda8f2c47fab6b35bad312f7
hmsrodrigues <hmsrodrigues@gmail.com>
2014-07-03 13:43:54 +0100

    .

--- p2pool/networks/isracoin.py
+++ p2pool/networks/isracoin.py
@@ -1,18 +1,18 @@
 from p2pool.bitcoin import networks
 
-        PARENT=networks.nets['isracoin']
-        SHARE_PERIOD=10 # seconds   
-        CHAIN_LENGTH=3*60*60//10 # shares 
-        REAL_CHAIN_LENGTH=3*60*60//10 # shares 
-        TARGET_LOOKBEHIND=20 # shares 
-        SPREAD=15 # blocks            
-        IDENTIFIER='77EC4F453456CD36'.decode('hex')
-        PREFIX='158c1f1f94949191'.decode('hex')
-        P2P_PORT=8331
-        MIN_TARGET=0
-        MAX_TARGET=2**256//2**20 - 1
-        PERSIST=False
-        WORKER_PORT=7331
-        BOOTSTRAP_ADDRS='p2pools.plentygadget.com'.split(' ')
-        ANNOUNCE_CHANNEL='#p2pool-alt'
-        VERSION_CHECK=lambda v: True
+PARENT=networks.nets['isracoin']
+SHARE_PERIOD=10 # seconds   
+CHAIN_LENGTH=3*60*60//10 # shares 
+REAL_CHAIN_LENGTH=3*60*60//10 # shares 
+TARGET_LOOKBEHIND=20 # shares 
+SPREAD=15 # blocks    
+IDENTIFIER='77EC4F453456CD36'.decode('hex')
+PREFIX='158c1f1f94949191'.decode('hex')
+P2P_PORT=8331
+MIN_TARGET=0
+MAX_TARGET=2**256//2**20 - 1
+PERSIST=False
+WORKER_PORT=7331
+BOOTSTRAP_ADDRS='p2pools.plentygadget.com'.split(' ')
+ANNOUNCE_CHANNEL='#p2pool-alt'
+VERSION_CHECK=lambda v: True
